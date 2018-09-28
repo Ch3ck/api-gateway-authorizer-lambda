daf3224c41c2ca77e40abaed2ee844f8f0847b5a
Nyah Check <check.nyah@gmail.com>
2018-09-28 10:48:16 -0700

    Add basic lambda enndpoint

---
+++ step1/basic_lambda_endpoint.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import json
+
+print('Loading basic lambda endpoint')
+
+
+def lambda_handler(event, context):
+    try:
+	    print("Hello world!")
+	    print("value1 = " + event['key1'])
+	    print("value2 = " + event['key2'])
+	    print("value3 = " + event['key3'])
+    
+    	return event['key1']  # Echo back the first key value
+
+    raise Exception('Something went wrong')
