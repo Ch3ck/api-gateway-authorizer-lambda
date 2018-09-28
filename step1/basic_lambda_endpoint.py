cb922b0eeea8a2e4ae1dccdb95887178882393d1
Nyah Check <check.nyah@gmail.com>
2018-09-28 11:01:23 -0700

    Fix issues with basic lambda

--- step1/basic_lambda_endpoint.py
+++ step1/basic_lambda_endpoint.py
@@ -6,12 +6,13 @@ print('Loading basic lambda endpoint')
 
 
 def lambda_handler(event, context):
-    try:
-	    print("Hello world!")
-	    print("value1 = " + event['key1'])
-	    print("value2 = " + event['key2'])
-	    print("value3 = " + event['key3'])
-    
-    	return event['key1']  # Echo back the first key value
+	try:
+		print("Hello world!")
+		print("value1 = " + event['key1'])
+		print("value2 = " + event['key2'])
+		print("value3 = " + event['key3'])
 
-    raise Exception('Something went wrong')
+		return event['key1']  # Echo back the first key value
+	except:
+		print('Something went wrong')
+		return None
