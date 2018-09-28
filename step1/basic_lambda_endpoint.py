20e362cb116dd26b7ab1a83e44763ee5b1598c3d
Nyah Check <check.nyah@gmail.com>
2018-09-28 13:27:40 -0700

    Update basic lamba func

--- step1/basic_lambda_endpoint.py
+++ step1/basic_lambda_endpoint.py
@@ -2,17 +2,24 @@ from __future__ import print_function
 
 import json
 
+
 print('Loading basic lambda endpoint')
 
-
 def lambda_handler(event, context):
-	try:
+    # TODO implement
+    try:
 		print("Hello world!")
 		print("value1 = " + event['key1'])
 		print("value2 = " + event['key2'])
 		print("value3 = " + event['key3'])
 
-		return event['key1']  # Echo back the first key value
-	except:
-		print('Something went wrong')
-		return None
+		return {
+		    "statusCode": 200,
+		    "body": event['key1']  # Echo back the first key value
+		}
+    except:
+        print('Something went wrong')
+        return {
+            "statusCode": 422,
+            "body": "Something went wrong"
+        }
